6f4df655dd152f9aaa51d75b3766909eecb9f9b9
Flavio Granero <maltempe@gmail.com>
2024-04-24 13:33:19 -0300

    install google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://gdg.londrina.dev',
+  site: "https://gdg.londrina.dev",
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
